384ce802bd9de104bf42eb3f75ff74928790b5a2
naglich19 <33611768+naglich19@users.noreply.github.com>
2018-07-20 18:43:28 -0400

    Update Main.java
    easy explanation

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 
 public class Main {		
 	public static void main(String[] args) {
-		for(int i=1; i<=100; i++) {
-			String x = " ";
+		
+		//loop statement to check numbers 1-100
+		for(int i=1; i<=100; i++) {			
+			
+			//creates empty string to put fizz or buzz into
+			String x = " ";			
+			
+			//checks if "i" is divisible by 3
 			if(i%3==0)
 				x += "fizz";
+			
+			//checks if "i" is divisible by 5
 			if(i%5==0)
-				x += "buzz";			
+				x += "buzz";
+			
+			//print whatever "i" has been tagged as
 			System.out.println(i +"= "+ x);	
 		}	
 	}	
